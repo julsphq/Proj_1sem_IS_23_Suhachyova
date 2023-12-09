ce6aec38d796c2052da4cf9977482345dc39bd06
julsphq <suhachyova.2005@gmail.com>
2023-12-09 18:37:52 +0300

    Practical work number 7

--- pz_7/pz_7_1.py
+++ pz_7/pz_7_1.py
@@ -1,16 +1,20 @@
 # Вариант 27. Дан символ C и строка S. Удвоить каждое вхождение символа C в строку S
-def double_char(s, c):
-    result = ""  # Создаем пустую строку для результата
-    for char in s:
-        if char == c:
-            result += char * 2
-        else:
-            result += char
+def main(C,S):
+    try:
+        temp_list = []
+        for char in S:
+            if not char == C:
+                temp_list.append(char)
+            else:
+                temp_list.append(char*2)
 
-    return result
+        our_str = ''.join(temp_list)
+        return our_str
 
-input_string = "Hello, World!"
-character = "o"
-doubled_string = double_char(input_string, character)
-print(doubled_string)
+    except:
+        print('Что-то пошло не так, попробуйте еще раз!')
+        print(main(C=input('Введите C: '), S=input('Введите S: ')))
 
+
+if __name__ == '__main__':
+    print(main(C=input('Введите C: '), S=input('Введите S: ')))
