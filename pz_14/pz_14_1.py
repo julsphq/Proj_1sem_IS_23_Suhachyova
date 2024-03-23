704eb8af993fe477ad844dc936717d739e503b49
julsphq <suhachyova.2005@gmail.com>
2024-03-23 13:51:22 +0300

    pz_14

--- pz_14/pz_14_1.py
+++ pz_14/pz_14_1.py
@@ -1,14 +1,18 @@
-#Вариант 27. Из исходного текстового файла (Dostoevsky.txt) выбрать блок информации за 1857
-#год и поместить ее в новый текстовый файл.
+#Вариант 27. Из исходного текстового файла (Dostoevsky.txt) выбрать информацию,которая начинается с 1857
+#года и поместить ее в новый текстовый файл
 
 import re
 
-with open('Dostoevsky.txt', encoding='utf-8') as file:
-    text = file.read()
+with open('Dostoevsky.txt', 'r', encoding='utf-8') as file:
+    data = file.read()
 
-pattern = re.compile(r"После .*", re.MULTILINE)
-matches = pattern.findall(text)
+pattern = re.compile(r'1857(.*?)(?=185\d{2}|$)', re.DOTALL)
+match = pattern.search(data)
 
-with open('newfile.txt', 'w', encoding='utf-8') as file:
-    file.writelines(matches)
-
+if match:
+    info_1857 = match.group(0)
+    with open('new_fille.txt', 'w', encoding='utf-8') as new_file:
+        new_file.write(info_1857)
+        print("Информация за 1857 год успешно сохранена в файл 'new_fille.txt'")
+else:
+    print("Информация за 1857 год не найдена в исходном файле")
