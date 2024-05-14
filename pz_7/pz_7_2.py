27abeb412adfbbf35c976b529a07564c024d6e54
julsphq <suhachyova.2005@gmail.com>
2024-05-14 21:56:09 +0300

    Editing a task 7.2

--- pz_7/pz_7_2.py
+++ pz_7/pz_7_2.py
@@ -1,8 +1,8 @@
 # Вариант 27. Дана строка, состоящая из русских слов, разделенных пробелами (одним или
 # несколькими). Найти количество слов в строке.
-def count_words(string):
+def main(string):
     string = string.split()
     print(f'Пробелов в строке: {len(string) - 1}')
 
 if __name__ == '__main__':
-    main(count_words= input('Введите строку: '))
+    main(string=input('Введите строку: '))
